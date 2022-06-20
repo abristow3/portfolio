c7ae31621d6b38d33841dccf60e9e7ee95a64e0e
Alex Bristow <alex.bristow3@gmail.com>
2022-06-20 12:29:05 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,111 @@
+const Typer = {
+    text: '',
+    accessCountimer: null,
+    index: 0,
+    speed: 2,
+    file: '',
+    accessCount: 0,
+    deniedCount: 0,
+    console: document.getElementById("console"),
+    init: function () {
+        accessCountimer = setInterval(function () {
+            Typer.updLstChr();
+        }, 500);
+        $.get(Typer.file, function (data) {
+            Typer.text = data;
+            Typer.text = Typer.text.slice(0, Typer.text.length - 1);
+        });
+    },
+
+    content: function () {
+        return $('#console').html();
+    },
+
+    write: function (str) {
+        $('#console').append(str);
+        return false;
+    },
+
+    addText: function (key) {
+        if (key.code === 18) {
+            Typer.accessCount++;
+
+            if (Typer.accessCount >= 3) {
+                Typer.makeAccess();
+            }
+        } else if (key.code === 20) {
+            Typer.deniedCount++;
+
+            if (Typer.deniedCount >= 3) {
+                Typer.makeDenied();
+            }
+        } else if (key.code === 27) {
+            Typer.hidepop();
+        } else if (Typer.text) {
+            const cont = Typer.content();
+            if (cont.substring(cont.length - 1, cont.length) === '|')
+                $('#console').html(
+                    $('#console')
+                        .html()
+                        .substring(0, cont.length - 1),
+                );
+            if (key.code !== 8) {
+                Typer.index += Typer.speed;
+            } else {
+                if (Typer.index > 0) Typer.index -= Typer.speed;
+            }
+            const text = Typer.text.substring(0, Typer.index);
+            const rtn = new RegExp('\n', 'g');
+
+            $('#console').html(text.replace(rtn, '<br/>'));
+            window.scrollBy(0, 50);
+        }
+
+        if (key.preventDefault && key.code !== 122) {
+            key.preventDefault();
+        }
+
+        if (key.code !== 122) {
+            // otherway prevent keys default behavior
+            key.returnValue = false;
+        }
+    },
+
+    updLstChr: function () {
+        const cont = this.content();
+
+        if (cont.substring(cont.length - 1, cont.length) === '|')
+            $('#console').html(
+                $('#console')
+                    .html()
+                    .substring(0, cont.length - 1),
+            );
+        else this.write('|'); // else write it
+    },
+};
+
+function replaceUrls(text) {
+    const http = text.indexOf('http://');
+    const space = text.indexOf('.me ', http);
+
+    if (space !== -1) {
+      const url = text.slice(http, space - 1);
+      return text.replace(url, '<a href="' + url + '">' + url + '</a>');
+  } else {
+    return text;
+  }
+}
+
+Typer.speed = 1;
+Typer.file = 'alex.txt';
+Typer.init();
+
+const timer = setInterval('t();', 30);
+
+function t() {
+  Typer.addText({ code: 123748 });
+
+  if (Typer.index > Typer.text.length) {
+    clearInterval(timer);
+  }
+}
